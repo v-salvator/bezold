060900d6e11050257635709f65b890cfa99f498c
Vincent <>
2024-06-30 20:25:44 +0800

    feat: using withConverter

--- firebase/clientUtils/store.ts
+++ firebase/clientUtils/store.ts
@@ -14,28 +14,30 @@ import type { Store, StoreDoc } from "@/types";
 
 const COLLECTION = "mockStore";
 
-const storeDecorator = (storeSnaphot: any) => {
-  const storeData = storeSnaphot.data() as StoreDoc;
-  return {
-    id: storeSnaphot.id,
-    ...storeData,
-    createTime: storeData?.createTime.toDate(),
-    updateTime: storeData?.updateTime.toDate(),
-  } as Store;
-};
+// const storeDecorator = (storeSnaphot: any) => {
+//   const storeData = storeSnaphot.data() as StoreDoc;
+//   return {
+//     id: storeSnaphot.id,
+//     ...storeData,
+//     createTime: storeData?.createTime.toDate(),
+//     updateTime: storeData?.updateTime.toDate(),
+//   } as Store;
+// };
 
-// * try to figure out how to use it Firestore data converter
 const storeConverter = {
+  // * try to figure out how to use toFirestore, dosn't seem to work maybe seems only work on setDoc not upateDoc
+  // * doc link: https://firebase.google.com/docs/reference/js/firestore_.firestoredataconverter
   toFirestore: (store: Store) => {
     return {
       ...store,
       updateTime: serverTimestamp(),
-      createTime: Timestamp.fromDate(store.createTime),
+      createTime: store.createTime
+        ? Timestamp.fromDate(store.createTime)
+        : serverTimestamp(),
     };
   },
   fromFirestore: (storeSnaphot: any) => {
     const storeData = storeSnaphot.data() as StoreDoc;
-    console.log("🚀 ~ storeData:", storeData);
     return {
       ...storeData,
       // * addons
@@ -61,22 +63,24 @@ export const createStoreDoc = async (store: Store) => {
 };
 
 export const getStores = async () => {
-  const querySnapshot = await getDocs(collection(db, COLLECTION));
+  const querySnapshot = await getDocs(
+    collection(db, COLLECTION).withConverter(storeConverter)
+  );
   const stores: Store[] = []; // TODO: modify the type here
 
   querySnapshot.forEach((docSnap) => {
-    const store = storeDecorator(docSnap);
+    const store = docSnap.data();
     stores.push(store);
   });
   return stores;
 };
 
 export const getStoreById = async (storeId: Store["id"]) => {
-  const docRef = doc(db, COLLECTION, storeId);
+  const docRef = doc(db, COLLECTION, storeId).withConverter(storeConverter);
   const docSnap = await getDoc(docRef);
 
   if (docSnap.exists()) {
-    const store = storeDecorator(docSnap);
+    const store = docSnap.data();
     return store;
   } else {
     // docSnap.data() will be undefined in this case
